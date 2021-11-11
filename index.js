8fc184f07d517cb92d4bdab0d44fca844bce5fe4
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-11 15:09:15 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 console.log("Hello World!!!!")
-
+// In javascript , we will not specify data type for the variables
+//Every thing in javascript is an object [ number, string, boolean,array ,function]
+// A variable in javascript can hold number,string,boolean, array,function, object
+a=90
+console.log(a)
+console.log(typeof(a))
+a="Rakesh"
+console.log(a)
+console.log(typeof(a))
+a=90.34
+console.log(a)
+console.log(typeof(a))
+a=true
+console.log(a)
+console.log(typeof(a))
+a=[1,2,3]
+console.log(a)
+console.log(typeof(a))
+a=new Array(1,2,3,5)
+console.log(a)
+console.log(typeof(a))
