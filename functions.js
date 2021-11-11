ebb4e4df4235c520a970af62aaeba469c72cb13c
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-11 16:39:08 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+function fun(a,b){
+  console.log(a,b)
+}
+
+fun("raj","kumar")
+fun(12,34)
+fun([1,2,3],[3,4,5])
+fun(12,"Raj")
+fun(true,false)
+fun(1,{sno:1,name:"Kiran"})
+fun()
+fun("Ram")
+fun("Ram","Rahim","Roger")
