a0e7e4b44e35cee644cc46dae2f63f3e7660a90b
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-16 17:02:21 +0530

    typescript

---
+++ Typescript/first.ts
@@ -0,0 +1,16 @@
+/* npm install -g typescript*/
+
+
+function display(a,b){
+    console.log(a,b)
+}
+
+display(123,353)
+display('Raj','Kumar')
+
+function display2(a:string,b:number):void{
+    console.log(a,b)
+}
+
+display2("Rahul",353)
+display2('Raj',12)
